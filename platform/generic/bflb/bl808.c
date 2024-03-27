020f9f4f82bd840e3908878dcbfbf62e3483b9b1
Peter J. Philipp <pbug44@delphinusdns.org>
2024-03-27 11:26:23 +0100

    try to atomically write both bits at one csr operation to mxstatus.

--- platform/generic/bflb/bl808.c
+++ platform/generic/bflb/bl808.c
@@ -12,8 +12,7 @@
 
 static int bl808_early_init(bool cold_boot, const struct fdt_match *match)
 {
-	csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(21));	/* MAEE */
-	csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(22));	/* THEADISAEE */
+	csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(21) | BIT(22));
 	csr_set(THEAD_C9XX_CSR_MHCR, 0x117f);
 	csr_set(THEAD_C9XX_CSR_MHINT, 0x304);
 
