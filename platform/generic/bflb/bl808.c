696420d5fe1d59438690dda79ae19cea909eee85
Peter J. Philipp <pbug44@delphinusdns.org>
2024-04-20 16:27:15 +0200

    long time no commit, I'm worried on always hitting CPU #1 when setting the MXSTATUS on the thead9xx which has 32-bit processors that don't understand this extended status it seems.
    So give it some workaround code to make sure we're on CPU #0 (the boot proc).
    Since I'm adding this on the final_init it may be too late but we'll see.
    If anything I'll add another commit to shove it into early_init.

--- platform/generic/bflb/bl808.c
+++ platform/generic/bflb/bl808.c
@@ -12,9 +12,25 @@
 
 static int bl808_early_init(bool cold_boot, const struct fdt_match *match)
 {
-	csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(15) | BIT(21) | BIT(22));
+	csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(15) | BIT(22));
 	csr_set(THEAD_C9XX_CSR_MHCR, 0x117f);
 	csr_set(THEAD_C9XX_CSR_MHINT, 0x304);
+
+	return 0;
+}
+
+static int bl808_final_init(bool cold_boot, const struct fdt_match *match)
+{
+	struct sbi_domain *dom = sbi_domain_thishart_ptr();
+	uint32 hart = current_hartid();
+
+	/*
+	 * the domain that is the root domain and is on the boot cpu
+	 * I think this is the 64-bit core in the bl-808
+	 */
+
+	if (dom->boot_hartid == hart)
+		csr_set(THEAD_C9XX_CSR_MXSTATUS, BIT(15) | BIT(21) | BIT(22));
 
 	return 0;
 }
@@ -27,4 +43,5 @@ static const struct fdt_match bl808_match[] = {
 const struct platform_override bl808 = {
 	.match_table	= bl808_match,
 	.early_init	= bl808_early_init,
+	.final_init	= bl808_final_init,
 };
